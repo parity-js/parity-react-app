a0db1a13f2609adc173246fc6fffd08c1625eebe
Nicolas Gotchac <ngotchac@gmail.com>
2018-01-26 18:20:41 +0100

    Add rewire dir to babel loader

--- packages/react-app-rewire-parity/src/inject-babel.js
+++ packages/react-app-rewire-parity/src/inject-babel.js
@@ -16,15 +16,17 @@
 
 const { getBabelLoader } = require('react-app-rewired');
 
-// const { createLoaderMatcher, findIndexAndRules } = require('./utils');
+const { createLoaderMatcher, findIndexAndRules } = require('./utils');
 
 module.exports = function injectBabel (config) {
-  // const { index, rules } = findIndexAndRules(config.module.rules, createLoaderMatcher('babel-loader'));
+  const { index, rules } = findIndexAndRules(config.module.rules, createLoaderMatcher('babel-loader'));
 
-  // rules[index].include = [].concat(
-  //   rules[index].include || [],
-  //   path.resolve(__dirname)
-  // );
+  // Add current directory to babel loader, used to compile the `parity-inject-script`
+  // script
+  rules[index].include = [].concat(
+    rules[index].include || [],
+    path.resolve(__dirname)
+  );
 
   const babelLoader = getBabelLoader(config.module.rules);
 
