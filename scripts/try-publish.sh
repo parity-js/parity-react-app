965c91cc20cd47197a0e34946d918d9400a13b18
Nicolas Gotchac <ngotchac@gmail.com>
2018-02-06 15:30:59 +0100

    Update Travis build

--- scripts/try-publish.sh
+++ scripts/try-publish.sh
@@ -5,7 +5,7 @@ set -e
 # pwd
 package=$(jq -r .name package.json)
 version=$(jq -r .version package.json)
-published=$(npm info $package@$version version)
+published=$(npm info $package version)
 
 if [[ "$version" = "$published" ]]; then
   echo "⚠️   $package@$version is already published!"
